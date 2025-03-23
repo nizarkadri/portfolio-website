405fd9d4c3deefbf9f5eb88dde8ab6574b06bc3b
nizarkadri <nizarkadri8@gmail.com>
2025-03-23 01:20:36 -0400

    Stop tracking .next directory

--- .next/server/app/api/skill-icons/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/skill-icons/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_next_1782c482._.js");
-runtime.loadChunk("server/chunks/[root of the server]__259c6c68._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/skill-icons/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/skill-icons/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/skill-icons/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"tL3maJGx25hA4E78LeHTDdwSeMfcgDjSo6s/8i1Fa4w=\"\n}"
