cd4cac5f4f999dba4d7b8aa2b531eceb71f09264
nizarkadri <nizarkadri8@gmail.com>
2025-06-01 01:27:54 -0400

    refactor: Update Resend initialization to use RESEND_API_KEY for improved clarity and consistency in API key management

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 type RecruiterData = {
   userType: 'recruiter';
